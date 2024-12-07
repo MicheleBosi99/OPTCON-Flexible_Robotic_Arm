44e5cbd011a54f69c2fc783d396276c2242c2900
MicheleBosi <michele.bosi99@gmail.com>
2024-12-07 17:11:08 +0100

    File di dichiarazione dei parametri. TODO: Valutare se posizionare qui i punti di equilibrio

---
+++ src_mike/config.py
@@ -0,0 +1,17 @@
+# Constants
+T = 10
+dt = 1e-4
+iterations = int(T/dt)
+
+# Dynamics parameters
+M1 = 2
+M2 = 2
+L1 = 1.5
+L2 = 1.5
+R1 = 0.75
+R2 = 0.75
+I1 = 1.5
+I2 = 1.5
+G  = 9.81
+F1 = 0.1
+F2 = 0.1
